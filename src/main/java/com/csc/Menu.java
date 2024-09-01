8f44c7d3e6bf3270e9d4c76fc6817e01e28b2562
JustinCHughes <hcjustin713@gmail.com>
2024-09-01 15:00:28 -0500

    Restructured program to better fit the parameters of the homework assignment and so that it returns the valid value

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -3,65 +3,59 @@ import java.util.Scanner;
 
 // Your code goes here!
 public class Menu
-{
-    // Aggregated all function manager options at the top
-    // LOWER_BOUND_ACTIVE and UPPER_BOUND_ACTIVE dictate if bounds will be placed on user input
-    // LOWER_BOUND and UPPER_BOUND state bound criterias
-    // Messages dictate what user sees when being prompted and the program's responses
-    private static boolean LOWER_BOUND_ACTIVE = true;
-    private static boolean UPPER_BOUND_ACTIVE = true;
-    private static int LOWER_BOUND = 0;
-    private static int UPPER_BOUND = 12;
-    private static String inputMessage = "Please enter a value: \n";
-    private static String errorMessage = "Input was invalid. Please try again.\n\n";
-    private static String successMessage = "Thank you. That input is valid. Have a nice day.\n";
-
-    public static void main(String[] args)
+{ 
+    public static int promptResponse(boolean upperBoundActive, boolean lowerBoundActive
+                                    ,int lowerBound, int upperBound, String inputMessage
+                                    ,String errorMessage, String successMessage)
     {
-        // Initializing check and Scanner
+        // Initializing variables
         boolean check = false;
-        
         Scanner in = new Scanner(System.in);
         System.out.print(inputMessage);
+        int userInput = 0;
         
-        while (check == false)
-        {
+        do {
             // Check if value if is int. If it is, call inputCheck to see if meets bound requirements
             if(in.hasNextInt())
             {
-                check = inputCheck(in.nextInt());
+                userInput = in.nextInt();
+                if (lowerBoundActive && upperBoundActive)
+                {
+                    check = userInput > lowerBound && userInput < upperBound;
+                }
+                else if (upperBoundActive)
+                {
+                    check = userInput < upperBound;
+                }
+                else if (lowerBoundActive)
+                {
+                    check = userInput > lowerBound;
+                }
+                else
+                {
+                    check = true;
+                }
             }
-
+            
             // If check is still false, re-request for input
             if(check == false)
             {
                 System.out.print(errorMessage + inputMessage);
                 in.nextLine();
             }
-        }
+        } while (check == false);
         // Value was true, terminate program
         System.out.print(successMessage);
+        
+        in.close();
+        return userInput;
     }
 
-    // Check if value submitted meets bound requirements
-    // Can always add more checks in here if needed such as divisibility
-    public static boolean inputCheck(int userInput)
+    public static void main(String[] args)
     {
-        if (UPPER_BOUND_ACTIVE && LOWER_BOUND_ACTIVE)
-        {
-            return userInput > LOWER_BOUND && userInput < UPPER_BOUND;
-        }
-        else if (UPPER_BOUND_ACTIVE)
-        {
-            return userInput < UPPER_BOUND;
-        }
-        else if (LOWER_BOUND_ACTIVE)
-        {
-            return userInput > LOWER_BOUND;
-        }
-        else
-        {
-            return true;
-        }
+        promptResponse(false, false, 0,100
+                        ,"Please enter a value: \n"
+                        ,"Input was invalid. Please try again.\n\n"
+                        ,"Thank you. That input is valid. Have a nice day.\n");
     }
 }
